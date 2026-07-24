85f1d1b35697a16c2646da3b8b6770fed27057d4
Dimpal Agarwal <alwardimpal12@gmail.com>
2026-07-24 17:32:24 +0530

    Time: 79 ms (39.02%) | Memory: 131.5 MB (30.32%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-        int n = profits.size(); 
-        int wt = w; 
-        int totalChose = 0; 
-        
-        map<int, vector<int>> mpp;
-        for(int i=0; i<n; i++){ 
-            mpp[capital[i]].push_back(profits[i]); 
-        } 
-        
-        priority_queue<int> pq; 
-        auto it = mpp.begin(); 
-        
-        while(totalChose != k){
-            while(it != mpp.end() && it->first <= wt) {
-                for(auto profit : it->second) {
-                    pq.push(profit);
-                }
-                it++;
+        int n = profits.size();
+
+        vector<pair<int, int>> projects;
+
+        for(int i = 0; i < n; i++){
+            projects.push_back({capital[i], profits[i]});
+        }
+
+        sort(projects.begin(), projects.end());
+        priority_queue<int> pq;
+
+        int i = 0;
+
+        while(k--){
+            while(i < n && projects[i].first <= w){
+                pq.push(projects[i].second);
+                i++;
             }
-            
-            if(pq.empty()) {
-                break; 
+            if (pq.empty()) {
+                break;
             }
-            
-            wt = wt + pq.top(); 
-            pq.pop(); 
-            totalChose += 1; 
-        } 
-        
-        return wt;
+            w += pq.top();
+            pq.pop();
+        }
+
+        return w;
     }
 };
