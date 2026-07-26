1b043022c6e44631b1359da49413ed62da2409ff
Dimpal Agarwal <alwardimpal12@gmail.com>
2026-07-26 22:21:38 +0530

    Time: 3 ms (35.19%) | Memory: 112.4 MB (62.25%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -3,30 +3,48 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
 
-        if(n==1 && gas[0]==2 && cost[0]==2) return 0;
+        // if(n==1 && gas[0]==2 && cost[0]==2) return 0;
 
-        int start = -1;
+        // int start = -1;
+        // for(int i=0; i<n; i++){
+        //     if(gas[i] > cost[i]){
+        //         bool check = true;
+        //         int point = gas[i]-cost[i];
+        //         int j = (i+1)%n;
+        //         while(j!=i){
+        //             point = point + gas[j] - cost[j];
+        //             if(point<0){
+        //                 check = false;
+        //                 break;
+        //             }
+        //             j = (j+1)%n;
+        //         }
+
+        //         if(check == true){
+        //             start = i;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // return start;
+
+        int totalGas = 0;
+        int totalCost = 0;
+        int currGas = 0;
+        int start = 0;
+
         for(int i=0; i<n; i++){
-            if(gas[i] > cost[i]){
-                bool check = true;
-                int point = gas[i]-cost[i];
-                int j = (i+1)%n;
-                while(j!=i){
-                    point = point + gas[j] - cost[j];
-                    if(point<0){
-                        check = false;
-                        break;
-                    }
-                    j = (j+1)%n;
-                }
+            totalGas += gas[i];
+            totalCost += cost[i];
 
-                if(check == true){
-                    start = i;
-                    break;
-                }
+            currGas += gas[i]-cost[i];
+            if(currGas < 0){
+                start = i+1;
+                currGas = 0;
             }
         }
 
-        return start;
+        return (totalGas < totalCost) ? -1 : start;
     }
 };
