47b44116af545071883e7558a9abf720a0ada4e6
Dimpal Agarwal <alwardimpal12@gmail.com>
2026-08-02 14:51:36 +0530

    Time: 325 ms (59.56%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
-select s1.score, (
-    select count(distinct s2.score) from Scores s2 where s2.score > s1.score
-)+1 AS `rank` from Scores s1 order by s1.score desc;
+
+#1st method
+-- select s1.score, (
+--     select count(distinct s2.score) from Scores s2 where s2.score > s1.score
+-- )+1 AS `rank` from Scores s1 order by s1.score desc;
+
+#2nd method
+select score, dense_rank() over(order by score desc) as `rank` from Scores;
