1ef18d379237bdba6b57707cd94affeb3f6948db
Dimpal Agarwal <alwardimpal12@gmail.com>
2026-08-17 02:17:36 +0530

    Time: 12 ms (12.25%) | Memory: 131.2 MB (93.11%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int n = stones.size();
+
+        int c0 = 0;
+        int c1 = 0;
+        int c2 = 0;
+
+        for(int i=0; i<n; i++){
+            if(stones[i]%3==0) c0++;
+            else if(stones[i]%3==1) c1++;
+            else c2++;
+        }
+
+        if(c0%2==0){
+            return c1!=0 && c2!=0;
+        }
+
+        return abs(c2-c1) >= 3;
+    }
+};
